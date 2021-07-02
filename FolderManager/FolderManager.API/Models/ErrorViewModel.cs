361a05a1b83c19b8af4db18de15e51b8424be1c4
Kateryna Riaba <w58989@student.wsiz.rzeszow.pl>
2021-07-02 18:15:55 +0200

    Initial commit

---
+++ FolderManager/FolderManager.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FolderManager.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
